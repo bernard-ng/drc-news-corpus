4a80175fa09fa6c559b752165d6c9a07ad75bfce
bernard-ng <ngandubernard@gmail.com>
2025-05-07 04:28:12 +0200

    fix: add missing options to open graph updater

--- bin/open-graph-sync.sh
+++ bin/open-graph-sync.sh
@@ -1,6 +1,6 @@
 #!/usr/local/cpanel/bin/jailshell
 
-SOURCES=("7sur7.cd" "actualite.cd" "beto.cd" "radiookapi.net" "mediacongo.net", "newscd.net")
+SOURCES=("7sur7.cd" "actualite.cd" "radiookapi.net" "mediacongo.net", "newscd.net")
 BASE_CMD="/usr/local/bin/php /home/eshimate/devscast.org/bin/console app:open-graph"
 LOG_DIR="/home/eshimate/devscast.org/var"
 
@@ -9,7 +9,7 @@ rm -f "${LOG_DIR}"/*.log
 
 for SOURCE in "${SOURCES[@]}"; do
     LOG_FILE="${LOG_DIR}/${SOURCE}.log"
-    nohup $BASE_CMD "$SOURCE" -vvv > "$LOG_FILE" 2>&1 &
+    nohup $BASE_CMD "$SOURCE" -vvv --no-interfaction > "$LOG_FILE" 2>&1 &
 done
 
 echo "All open graph crawlers started in the background."
