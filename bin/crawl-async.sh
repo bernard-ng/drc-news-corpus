539e230534714e8317324ad6fd088cd129da3ffc
bernard-ng <ngandubernard@gmail.com>
2025-06-16 21:57:26 +0200

    style: shellcheck

--- bin/crawl-async.sh
+++ bin/crawl-async.sh
@@ -1,8 +1,35 @@
 #!/usr/bin/env bash
 
-SOURCES=("africanewsrdc.net" "angazainstitute.ac.cd" "b-onetv.cd" "bukavufm.com" "changement7.net" "congoactu.net" "congoindependant.com" "congoquotidien.com" "cumulard.cd" "environews-rdc.net" "freemediardc.info" "geopolismagazine.org" "habarirdc.net" "infordc.com" "kilalopress.net" "laprosperiteonline.net" "laprunellerdc.cd" "lesmedias.net" "lesvolcansnews.net" "netic-news.net" "objectif-infos.cd" "scooprdc.net" "journaldekinshasa.com" "lepotentiel.cd" "acturdc.com" "matininfos.net")
-BASE_CMD="/usr/bin/php /home/ubuntu/drc-news-corpus/bin/console app:crawl"
-LOG_DIR="/home/ubuntu/drc-news-corpus/var"
+SOURCES=(
+  "africanewsrdc.net"
+  "angazainstitute.ac.cd"
+  "b-onetv.cd"
+  "bukavufm.com"
+  "changement7.net"
+  "congoactu.net"
+  "congoindependant.com"
+  "congoquotidien.com"
+  "cumulard.cd"
+  "environews-rdc.net"
+  "freemediardc.info"
+  "geopolismagazine.org"
+  "habarirdc.net"
+  "infordc.com"
+  "kilalopress.net"
+  "laprosperiteonline.net"
+  "laprunellerdc.cd"
+  "lesmedias.net"
+  "lesvolcansnews.net"
+  "netic-news.net"
+  "objectif-infos.cd"
+  "scooprdc.net"
+  "journaldekinshasa.com"
+  "lepotentiel.cd"
+  "acturdc.com"
+  "matininfos.net"
+)
+BASE_CMD="/usr/bin/php /var/www/html/news.devscast.tech/bin/console app:crawl"
+LOG_DIR="/var/www/html/news.devscast.tech/var"
 
 mkdir -p "$LOG_DIR"
 rm -f "${LOG_DIR}"/*.log
